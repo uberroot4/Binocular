35532651d9b9ba980130391ea9b4d5ee793b37a8
Manuel Stöger <manuel.stoeger@inso-world.com>
2025-03-24 00:03:47 +0100

    Initial Commit new backend

---
+++ binocular-backend-new/web/src/main/kotlin/com/inso_world/binocular/web/BinocularWebApplication.kt
@@ -0,0 +1,11 @@
+package com.inso_world.binocular.web
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BinocularWebApplication
+
+fun main(args: Array<String>) {
+	runApplication<BinocularWebApplication>(*args)
+}
---
+++ binocular-backend-new/web/src/test/kotlin/com/inso_world/binocular/web/BinocularWebApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.inso_world.binocular.web
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BinocularWebApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
