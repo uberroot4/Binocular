53ee3f577e96a7bafbdcae15ad7d05dd172d76e9
12225087 <e12225087@student.tuwien.ac.at>
2025-05-22 15:47:47 +0200

    Created the basic structure for the visualization

---
+++ binocular-frontend/src/visualizations/change-frequency/index.ts
@@ -0,0 +1,27 @@
+import ChartComponent from './chart';
+import ConfigComponent from './config';
+import HelpComponent from './help';
+import saga from './sagas';
+import reducer from './reducers';
+
+interface VisualizationModule {
+  id: string;
+  label: string;
+  saga: any;
+  reducer: any;
+  ChartComponent: React.ComponentType<any>;
+  ConfigComponent: React.ComponentType<any>;
+  HelpComponent: React.ComponentType<any>;
+}
+
+const changeFrequencyVisualization: VisualizationModule = {
+  id: 'changeFrequency',
+  label: 'Change Frequency',
+  saga,
+  reducer,
+  ChartComponent,
+  ConfigComponent,
+  HelpComponent
+};
+
+export default changeFrequencyVisualization; 
