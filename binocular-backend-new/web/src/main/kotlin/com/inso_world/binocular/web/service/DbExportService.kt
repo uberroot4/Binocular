842ed56f33e1cbc83e278bd56ffca379b3adfbdc
kkastenmeier <kai.kastenmeier@gmail.com>
2025-05-28 17:41:27 +0200

    #308 Controller and service class for export endpoint added classes and basic structure for the db export endpoint

---
+++ binocular-backend-new/web/src/main/kotlin/com/inso_world/binocular/web/service/DbExportService.kt
@@ -0,0 +1,17 @@
+package com.inso_world.binocular.web.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class DbExportService {
+
+  fun exportDb(): Map<String, Any> {
+    val exportJson = mutableMapOf<String, Any>()
+
+    // TODO: add the logic
+
+
+    return exportJson
+  }
+
+}
