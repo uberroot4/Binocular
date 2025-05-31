ee689db50a4b30820c035bc103702c96660c0430
Bene-dykt <eckerstorfer.benedikt@gmail.com>
2025-05-31 15:15:43 +0200

    data: add mock data implementation to MockData
    ref: #286

--- binocular-frontend-new/src/plugins/dataPlugins/mockData/src/index.ts
+++ binocular-frontend-new/src/plugins/dataPlugins/mockData/src/index.ts
@@ -1,14 +1,15 @@
-import Commits from './commits.ts';
-import { DataPlugin } from '../../../interfaces/dataPlugin.ts';
-import Users from './users.ts';
-import General from './general.ts';
-import Files from './files.ts';
-import Builds from './builds.ts';
+import Commits from "./commits.ts";
+import { DataPlugin } from "../../../interfaces/dataPlugin.ts";
+import Users from "./users.ts";
+import General from "./general.ts";
+import Files from "./files.ts";
+import Builds from "./builds.ts";
+import AccountsIssues from "./accounts-issues.ts";
 
 class MockData implements DataPlugin {
-  public name = 'Mock Data';
-  public description = 'Mocked Data for testing purposes.';
-  public capabilities = ['authors', 'commits', 'files'];
+  public name = "Mock Data";
+  public description = "Mocked Data for testing purposes.";
+  public capabilities = ["authors", "commits", "files"];
   public experimental = false;
   public requirements = {
     apiKey: false,
@@ -21,6 +22,7 @@ class MockData implements DataPlugin {
   public users;
   public general;
   public files;
+  public accountsIssues;
 
   constructor() {
     this.commits = new Commits();
@@ -28,6 +30,7 @@ class MockData implements DataPlugin {
     this.users = new Users();
     this.general = new General();
     this.files = new Files();
+    this.accountsIssues = new AccountsIssues();
   }
 
   public async init() {}
