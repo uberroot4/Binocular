8cba902513756319624f59560ca49f89b771a8db
12225087 <e12225087@student.tuwien.ac.at>
2025-07-14 14:58:13 +0200

    feat: connected the binocular backend to the visualization

--- binocular-frontend-new/src/plugins/dataPlugins/binocularBackend/src/commits.ts
+++ binocular-frontend-new/src/plugins/dataPlugins/binocularBackend/src/commits.ts
@@ -52,4 +52,71 @@ export default class Commits implements DataPluginCommits {
     const allCommits = commitList.sort((a, b) => new Date(b.date).getMilliseconds() - new Date(a.date).getMilliseconds());
     return allCommits.filter((c) => new Date(c.date) >= new Date(from) && new Date(c.date) <= new Date(to));
   }
+
+  public async getCommitDataWithFilesAndOwnership(commitSpan: [Date, Date], significantSpan: [Date, Date]) {
+    const commitList: any[] = [];
+    const significantSince = significantSpan[0];
+    const significantUntil = significantSpan[1];
+
+    const getCommitsPage = (until: Date) => async (page: number, perPage: number) => {
+      const resp = await this.graphQl.client.query({
+        query: gql`
+          query ($page: Int, $perPage: Int, $until: Timestamp) {
+            commits(page: $page, perPage: $perPage, until: $until) {
+              count
+              page
+              perPage
+              data {
+                sha
+                branch
+                message
+                signature
+                webUrl
+                date
+                parents
+                stats {
+                  additions
+                  deletions
+                }
+                files {
+                  data {
+                    file {
+                      path
+                    }
+                    lineCount
+                    ownership {
+                      user
+                      hunks {
+                        originalCommit
+                        lines {
+                          from
+                          to
+                        }
+                      }
+                    }
+                    stats {
+                      additions
+                      deletions
+                    }
+                    hunks {
+                      newLines
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `,
+        variables: { page, perPage, until },
+      });
+      return resp.data.commits;
+    };
+
+    await traversePages(getCommitsPage(significantUntil), (commit: any) => {
+      commitList.push(commit);
+    });
+
+    const allCommits = commitList.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+    return allCommits.filter((c) => new Date(c.date) >= new Date(significantSince));
+  }
 }
