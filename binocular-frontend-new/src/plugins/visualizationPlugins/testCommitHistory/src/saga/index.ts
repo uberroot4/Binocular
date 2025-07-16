9d0a1130351b9f934fbf1342debfb2c542e6a336
martin <martin.weber.at@gmail.com>
2025-07-16 08:32:33 +0200

    feat(260.1): created saga for test commit history plugin

---
+++ binocular-frontend-new/src/plugins/visualizationPlugins/testCommitHistory/src/saga/index.ts
@@ -0,0 +1,41 @@
+import { put, takeEvery, fork, call, select } from 'redux-saga/effects';
+import {
+  DataState,
+  setCommits,
+  setCommitsFilesConnections,
+  setDataState,
+  setDateRange,
+  setFiles,
+  TestCommitHistoryState,
+} from '../reducer';
+import { DataPlugin } from '../../../../interfaces/dataPlugin.ts';
+import { DataPluginFiles } from '../../../../interfaces/dataPluginInterfaces/dataPluginFiles.ts';
+import { DataPluginCommit } from '../../../../interfaces/dataPluginInterfaces/dataPluginCommits.ts';
+import { DataPluginCommitsFilesConnection } from '../../../../interfaces/dataPluginInterfaces/dataPluginCommitsFilesConnections.ts';
+
+export default function* (dataConnection: DataPlugin) {
+  yield fork(() => watchRefresh(dataConnection));
+  yield fork(() => watchDateRangeChange(dataConnection));
+}
+
+function* watchRefresh(dataConnection: DataPlugin) {
+  yield takeEvery('REFRESH', () => fetchChangesData(dataConnection));
+}
+
+function* watchDateRangeChange(dataConnection: DataPlugin) {
+  yield takeEvery(setDateRange, () => fetchChangesData(dataConnection));
+}
+
+function* fetchChangesData(dataConnection: DataPlugin) {
+  yield put(setDataState(DataState.FETCHING));
+  const state: TestCommitHistoryState = yield select();
+  const commits: DataPluginCommit[] = yield call(() => dataConnection.commits.getAll(state.dateRange.from, state.dateRange.to));
+  yield put(setCommits(commits));
+  const files: DataPluginFiles[] = yield call(() => dataConnection.files.getAll());
+  yield put(setFiles(files));
+  const commitsFilesConnections: DataPluginCommitsFilesConnection[] = yield call(() =>
+    dataConnection.commitsFilesConnections.getAll(state.dateRange.from, state.dateRange.to),
+  );
+  yield put(setCommitsFilesConnections(commitsFilesConnections));
+  yield put(setDataState(DataState.COMPLETE));
+}
