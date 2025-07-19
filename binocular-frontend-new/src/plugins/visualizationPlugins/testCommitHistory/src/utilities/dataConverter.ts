7455020fcd32530b58eae513db1b718b48b76f23
martin <martin.weber.at@gmail.com>
2025-07-19 07:35:49 +0200

    refactor(260.1): simplify test commit data creation by optimizing file filtering and connection mapping

--- binocular-frontend-new/src/plugins/visualizationPlugins/testCommitHistory/src/utilities/dataConverter.ts
+++ binocular-frontend-new/src/plugins/visualizationPlugins/testCommitHistory/src/utilities/dataConverter.ts
@@ -1,34 +1,34 @@
 import { DataPluginCommit } from '../../../../interfaces/dataPluginInterfaces/dataPluginCommits.ts';
-import { DataPluginFile, DataPluginFiles } from '../../../../interfaces/dataPluginInterfaces/dataPluginFiles.ts';
+import { DataPluginFile } from '../../../../interfaces/dataPluginInterfaces/dataPluginFiles.ts';
 import { DataPluginCommitsFilesConnection } from '../../../../interfaces/dataPluginInterfaces/dataPluginCommitsFilesConnections.ts';
 import { TestEvolutionChartData } from '../chart/chart.tsx';
 
 export function createBarCharData(
   commits: DataPluginCommit[],
-  files: DataPluginFiles[],
+  files: DataPluginFile[],
   commitsFilesConnections: DataPluginCommitsFilesConnection[],
 ): TestEvolutionChartData[] {
-  const testFiles: DataPluginFiles[] = files.filter(
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
+  const testFiles: DataPluginFile[] = files.filter(
     (file: DataPluginFile) => file.path.includes('src/test/') && !file.path.includes('src/test/resources/'),
   );
 
   const sortedCommitsByDate = [...commits].sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
 
   if (sortedCommitsByDate.length > 0) {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-    return sortedCommitsByDate.map((commit: DataPluginCommit) => ({
-      time: commit.date.split('T')[0],
-      amountOfTestCommits: testFiles.reduce(
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-expect-error
-        (count: number, testFile: DataPluginFile) =>
-          count + commitsFilesConnections.filter((connection) => connection._from === commit._id && connection._to === testFile._id).length,
-        0,
-      ),
-    }));
+    return sortedCommitsByDate.map((commit: DataPluginCommit) => {
+      const commitDate = commit.date?.split('T')[0];
+      const relevantConnections = commitsFilesConnections.filter((connection) => connection._from === commit._id);
+
+      const amount = testFiles.reduce((count, testFile: DataPluginFile) => {
+        const matchedConnections = relevantConnections.filter((connection) => connection._to === testFile._id);
+        return count + matchedConnections.length;
+      }, 0);
+
+      return {
+        time: commitDate,
+        amountOfTestCommits: amount,
+      };
+    });
   }
 
   return [];
