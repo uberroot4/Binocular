239e94cc38b277341c5e6efa0e2a9210971607a1
martin <martin.weber.at@gmail.com>
2025-07-24 17:28:47 +0200

    refactor(260.1): remove console log from getSVGData function

--- binocular-frontend-new/src/plugins/visualizationPlugins/testCommitHistory/src/utilities/utilities.ts
+++ binocular-frontend-new/src/plugins/visualizationPlugins/testCommitHistory/src/utilities/utilities.ts
@@ -1,7 +1,6 @@
 import { RefObject } from 'react';
 
 export function getSVGData(chartContainerRef: RefObject<HTMLDivElement | undefined>): string {
-  console.log(chartContainerRef.current?.children[0].outerHTML);
   const svgData = chartContainerRef.current?.children[0].outerHTML;
   if (svgData === undefined) {
     return '<svg xmlns="http://www.w3.org/2000/svg"></svg>';
