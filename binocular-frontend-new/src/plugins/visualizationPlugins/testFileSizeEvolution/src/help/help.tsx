48c1b142f92b0e065068859eafce49b5dc1785bf
martin <martin.weber.at@gmail.com>
2025-07-24 16:09:33 +0200

    feat(260.3): add help, reducer, saga, settings, assets and plugin components for the testFileSizeEvolution plugin

---
+++ binocular-frontend-new/src/plugins/visualizationPlugins/testFileSizeEvolution/src/help/help.tsx
@@ -0,0 +1,18 @@
+function Help() {
+  return (
+    <>
+      <h2>Test File Size Evolution Help</h2>
+      <h3>Data</h3>
+      <b>Note: This plugin is only compatible with Java projects that use Maven and Spring Boot.</b>
+      <br></br>
+      <br></br>
+      <p>
+        This plugin visualizes the evolution of test file sizes over time, providing insights into how the size of test files changes with
+        each commit. It helps developers understand the growth or reduction of test code, which can be crucial for maintaining test quality
+        and performance.
+      </p>
+    </>
+  );
+}
+
+export default Help;
