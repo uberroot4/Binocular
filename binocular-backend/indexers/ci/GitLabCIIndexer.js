40e38378f02ee936be78761e5729c8d60da858fa
Maximilian Zenz <maximilian.johannes.zenz@gmail.com>
2025-07-25 08:54:11 +0200

    #327 (New Frontend) Port Time Spent Visualization:
    - fix backend unit test

--- binocular-backend/indexers/ci/GitLabCIIndexer.js
+++ binocular-backend/indexers/ci/GitLabCIIndexer.js
@@ -22,7 +22,7 @@ class GitLabCIIndexer extends BaseGitLabIndexer {
       pipeline.jobs = jobs.map((job) => ({
         id: job.id.replace('gid://gitlab/Ci::Build/', ''),
         name: job.name,
-        status: job.status.lower,
+        status: job.status.toLowerCase(),
         stage: job.stage.name,
         createdAt: job.createdAt,
         finishedAt: job.finishedAt,
