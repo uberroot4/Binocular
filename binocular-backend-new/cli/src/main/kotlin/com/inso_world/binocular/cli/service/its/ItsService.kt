869977563f591fd519cd2c21db4b343d8fe49663
kkastenmeier <kai.kastenmeier@gmail.com>
2025-08-16 15:27:21 +0200

    #330 Added ItsService class.

---
+++ binocular-backend-new/cli/src/main/kotlin/com/inso_world/binocular/cli/service/its/ItsService.kt
@@ -0,0 +1,16 @@
+package com.inso_world.binocular.cli.service.its
+
+import com.inso_world.binocular.cli.service.VcsService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+/**
+ * Service for indexing ITS data.
+ */
+@Service
+class ItsService {
+    private var logger: Logger = LoggerFactory.getLogger(VcsService::class.java)
+
+    //TODO
+}
