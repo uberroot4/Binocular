98a4fe66a3837cea8ae4590751353b700dcc5388
kkastenmeier <kai.kastenmeier@gmail.com>
2025-08-17 14:27:49 +0200

    #330 Added logging and error handling to GitHubService.

--- binocular-backend-new/cli/src/main/kotlin/com/inso_world/binocular/cli/service/its/GitHubService.kt
+++ binocular-backend-new/cli/src/main/kotlin/com/inso_world/binocular/cli/service/its/GitHubService.kt
@@ -1,9 +1,12 @@
 package com.inso_world.binocular.cli.service.its
 
 import com.inso_world.binocular.cli.config.BinocularRcLoader
+import com.inso_world.binocular.cli.exception.ServiceException
 import com.inso_world.binocular.cli.index.its.GitHubUser
 import com.inso_world.binocular.cli.index.its.GraphQlUserResponse
 import com.inso_world.binocular.cli.index.its.PageInfo
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
@@ -14,7 +17,10 @@ import reactor.core.publisher.Mono
 @Service
 class GitHubService (private val webClient: WebClient, private val configLoader: BinocularRcLoader) {
 
+    private var logger: Logger = LoggerFactory.getLogger(GitHubService::class.java)
+
     fun loadAllAssignableUsers(owner: String, repo: String): Mono<List<GitHubUser>> {
+        logger.trace("Load all assignable users from GitHub for $owner $repo")
         val allUsers = mutableListOf<GitHubUser>()
 
         fun fetchPage(cursor: String?): Mono<Pair<List<GitHubUser>, PageInfo>> {
@@ -47,10 +53,17 @@ class GitHubService (private val webClient: WebClient, private val configLoader:
                 .header("Authorization", "Bearer ${configLoader.loadGitHubToken()}")
                 .bodyValue(request)
                 .retrieve()
+                .onStatus({ it.is4xxClientError || it.is5xxServerError }) { response ->
+                    logger.error("GitHub API returned error: ${response.statusCode()}")
+                    response.bodyToMono(String::class.java).map { body ->
+                        ServiceException("GitHub API error ${response.statusCode()}: $body")
+                    }
+                }
                 .bodyToMono(GraphQlUserResponse::class.java)
                 .map { res ->
                     val users = res.data.repository.assignableUsers.nodes
                     val pageInfo = res.data.repository.assignableUsers.pageInfo
+                    logger.info("Received ${users.size} users, hasNextPage= ${pageInfo.hasNextPage}")
                     users to pageInfo
                 }
         }
