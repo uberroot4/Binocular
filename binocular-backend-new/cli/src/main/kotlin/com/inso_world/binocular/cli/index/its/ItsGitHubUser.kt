3e99fcac5220d45b3a94dd7f14fb4934b435845e
kkastenmeier <kai.kastenmeier@gmail.com>
2025-08-18 19:11:02 +0200

    #330 Added toDomain mapper in ItsGitHubUser.

--- binocular-backend-new/cli/src/main/kotlin/com/inso_world/binocular/cli/index/its/ItsGitHubUser.kt
+++ binocular-backend-new/cli/src/main/kotlin/com/inso_world/binocular/cli/index/its/ItsGitHubUser.kt
@@ -1,9 +1,23 @@
 package com.inso_world.binocular.cli.index.its
 
+import com.inso_world.binocular.model.Account
+import com.inso_world.binocular.model.Platform
+
 data class ItsGitHubUser(
     val login: String,
     //val email: String?,
     val name: String?,
     val url: String?,
     val avatarUrl: String?
-)
+) {
+    fun toDomain(): Account {
+        val acc = Account(
+            login = this.login,
+            name = this.name,
+            url = this.url,
+            avatarUrl = this.avatarUrl,
+            platform = Platform.GitHub
+        )
+        return acc
+    }
+}
