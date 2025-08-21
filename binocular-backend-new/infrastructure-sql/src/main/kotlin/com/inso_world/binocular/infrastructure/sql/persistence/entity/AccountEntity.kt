d4717d551c1d0bbb4b30a6f0641866dbd70f73df
kkastenmeier <kai.kastenmeier@gmail.com>
2025-08-21 13:34:14 +0200

    #330 Added AccountEntity and functions for uniqueKey, equals and hashCode.

--- binocular-backend-new/infrastructure-sql/src/main/kotlin/com/inso_world/binocular/infrastructure/sql/persistence/entity/AccountEntity.kt
+++ binocular-backend-new/infrastructure-sql/src/main/kotlin/com/inso_world/binocular/infrastructure/sql/persistence/entity/AccountEntity.kt
@@ -1,47 +1,62 @@
-// package com.inso_world.binocular.infrastructure.sql.persistence.entity
-//
-// import com.inso_world.binocular.model.Platform
-// import jakarta.persistence.Column
-// import jakarta.persistence.Entity
-// import jakarta.persistence.EnumType
-// import jakarta.persistence.Enumerated
-// import jakarta.persistence.Id
-// import jakarta.persistence.Table
-//
-// /**
-// * SQL-specific Account entity.
-// */
-// @Entity
-// @Table(name = "accounts")
-// data class AccountEntity(
-//    @Id
-//    var id: Long? = null,
-//    @Enumerated(EnumType.STRING)
-//    var platform: Platform? = null,
-//    var login: String? = null,
-//    var name: String? = null,
-//    @Column(name = "avatar_url")
-//    var avatarUrl: String? = null,
-//    var url: String? = null,
-// //    @ManyToMany
-// //    @JoinTable(
-// //        name = "issue_account_connections",
-// //        joinColumns = [JoinColumn(name = "account_id")],
-// //        inverseJoinColumns = [JoinColumn(name = "issue_id")],
-// //    )
-// //    var issues: MutableList<IssueEntity> = mutableListOf(),
-// //    @ManyToMany
-// //    @JoinTable(
-// //        name = "merge_request_account_connections",
-// //        joinColumns = [JoinColumn(name = "account_id")],
-// //        inverseJoinColumns = [JoinColumn(name = "merge_request_id")],
-// //    )
-// //    var mergeRequests: MutableList<MergeRequestEntity> = mutableListOf(),
-// //    @ManyToMany
-// //    @JoinTable(
-// //        name = "note_account_connections",
-// //        joinColumns = [JoinColumn(name = "account_id")],
-// //        inverseJoinColumns = [JoinColumn(name = "note_id")],
-// //    )
-// //    var notes: MutableList<NoteEntity> = mutableListOf(),
-// )
+ package com.inso_world.binocular.infrastructure.sql.persistence.entity
+
+ import com.inso_world.binocular.model.Platform
+ import jakarta.persistence.Column
+ import jakarta.persistence.Entity
+ import jakarta.persistence.EnumType
+ import jakarta.persistence.Enumerated
+ import jakarta.persistence.Id
+ import jakarta.persistence.Table
+ import java.util.Objects
+
+ /**
+ * SQL-specific Account entity.
+ */
+ @Entity
+ @Table(name = "accounts")
+ data class AccountEntity(
+    @Id
+    var id: Long? = null,
+    @Enumerated(EnumType.STRING)
+    val platform: Platform,
+    val login: String,
+    var name: String? = null,
+    @Column(name = "avatar_url")
+    var avatarUrl: String? = null,
+    var url: String? = null,
+ //    @ManyToMany
+ //    @JoinTable(
+ //        name = "issue_account_connections",
+ //        joinColumns = [JoinColumn(name = "account_id")],
+ //        inverseJoinColumns = [JoinColumn(name = "issue_id")],
+ //    )
+ //    var issues: MutableList<IssueEntity> = mutableListOf(),
+ //    @ManyToMany
+ //    @JoinTable(
+ //        name = "merge_request_account_connections",
+ //        joinColumns = [JoinColumn(name = "account_id")],
+ //        inverseJoinColumns = [JoinColumn(name = "merge_request_id")],
+ //    )
+ //    var mergeRequests: MutableList<MergeRequestEntity> = mutableListOf(),
+ //    @ManyToMany
+ //    @JoinTable(
+ //        name = "note_account_connections",
+ //        joinColumns = [JoinColumn(name = "account_id")],
+ //        inverseJoinColumns = [JoinColumn(name = "note_id")],
+ //    )
+ //    var notes: MutableList<NoteEntity> = mutableListOf(),
+     ) : AbstractEntity() {
+
+     override fun uniqueKey(): String {
+         return "${this.platform}:${this.login}"
+     }
+
+     override fun equals(other: Any?): Boolean {
+         if (this === other) return true
+         if (other !is AccountEntity) return false
+         return this.uniqueKey() == other.uniqueKey()
+     }
+
+     override fun hashCode(): Int = Objects.hashCode("${this.platform}:${this.login}")
+
+ }
