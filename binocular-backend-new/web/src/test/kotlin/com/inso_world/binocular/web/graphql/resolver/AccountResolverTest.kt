d426f28e086a4357df15ae3448ef71762c4cae56
kkastenmeier <kai.kastenmeier@gmail.com>
2025-08-28 15:30:31 +0200

    #330 Fixed test by adding gid.

--- binocular-backend-new/web/src/test/kotlin/com/inso_world/binocular/web/graphql/resolver/AccountResolverTest.kt
+++ binocular-backend-new/web/src/test/kotlin/com/inso_world/binocular/web/graphql/resolver/AccountResolverTest.kt
@@ -200,7 +200,7 @@ internal class AccountResolverTest : GraphQlControllerTest() {
         @Test
         fun `issues should return empty list when account id is null`() {
             // Arrange
-            val account = Account(id = null, platform = Platform.GitHub, login = "test-user", name = "Test User")
+            val account = Account(id = null, gid = "AJDLJFLASfeD23048703", platform = Platform.GitHub, login = "test-user", name = "Test User")
 
             // Act
             val result = accountResolver.issues(account)
@@ -212,7 +212,7 @@ internal class AccountResolverTest : GraphQlControllerTest() {
         @Test
         fun `mergeRequests should return empty list when account id is null`() {
             // Arrange
-            val account = Account(id = null, platform = Platform.GitHub, login = "test-user", name = "Test User")
+            val account = Account(id = null, gid = "AJDLJFasSKJD23048703", platform = Platform.GitHub, login = "test-user", name = "Test User")
 
             // Act
             val result = accountResolver.mergeRequests(account)
@@ -224,7 +224,7 @@ internal class AccountResolverTest : GraphQlControllerTest() {
         @Test
         fun `notes should return empty list when account id is null`() {
             // Arrange
-            val account = Account(id = null, platform = Platform.GitHub, login = "test-user", name = "Test User")
+            val account = Account(id = null, gid = "AJDJFeKJD6423048703", platform = Platform.GitHub, login = "test-user", name = "Test User")
 
             // Act
             val result = accountResolver.notes(account)
