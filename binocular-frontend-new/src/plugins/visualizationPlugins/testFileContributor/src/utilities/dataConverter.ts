91c419adc183209eb467972a59927a53b319f9b7
martin <martin.weber.at@gmail.com>
2025-08-30 11:24:02 +0200

    feat(260.4): enhance pie chart data creation with author filtering and settings integration

--- binocular-frontend-new/src/plugins/visualizationPlugins/testFileContributor/src/utilities/dataConverter.ts
+++ binocular-frontend-new/src/plugins/visualizationPlugins/testFileContributor/src/utilities/dataConverter.ts
@@ -4,6 +4,9 @@ import { DataPluginCommitsFilesConnection } from '../../../../interfaces/dataPlu
 import { TestFileContributorChartData } from '../chart/chart.tsx';
 import { DataPluginUser } from '../../../../interfaces/dataPluginInterfaces/dataPluginUsers.ts';
 import { DataPluginCommitsUsersConnection } from '../../../../interfaces/dataPluginInterfaces/dataPluginCommitsUsersConnections.ts';
+import { AuthorType } from '../../../../../types/data/authorType.ts';
+import { ParametersType } from '../../../../../types/parameters/parametersType.ts';
+import { TestFileContributorSettings } from '../settings/settings.tsx';
 
 export function createPieChartData(
   commits: DataPluginCommit[],
@@ -11,60 +14,85 @@ export function createPieChartData(
   users: DataPluginUser[],
   commitsFilesConnections: DataPluginCommitsFilesConnection[],
   commitsUsersConnections: DataPluginCommitsUsersConnection[],
-  excludeMergeCommits: boolean,
-  commitType: string,
+  parameters: ParametersType,
+  settings: TestFileContributorSettings,
+  authorList: AuthorType[],
 ): TestFileContributorChartData[] {
-  // Filter for test files
+  if (commits.length === 0) return []; // Early return if no commits are available
+  if (authorList.length === 0) return []; // Early return if no authors are available
+
+  // Filter authors based on selection and parent status
+  const selectedAuthors: AuthorType[] = authorList.filter((author: AuthorType) => author.selected && author.parent == -1);
+  const mergedAuthors: AuthorType[] = authorList.filter((author: AuthorType) => author.selected && author.parent > 0);
+
+  if (selectedAuthors.length === 0) return []; // Early return if no authors are selected
+
+  // Filter files for test files
   const testFiles: DataPluginFile[] = files.filter(
     (file: DataPluginFile) => file.path.includes('src/test/') && !file.path.includes('src/test/resources/'),
   );
-  // Sort commits by date ascending
-  let sortedCommitsByDate: DataPluginCommit[] = [...commits].sort(
-    (a: DataPluginCommit, b: DataPluginCommit) => new Date(a.date).getTime() - new Date(b.date).getTime(),
-  );
+
   // Filter out commits that are merge commits if the flag is set
-  if (excludeMergeCommits) {
-    sortedCommitsByDate = sortedCommitsByDate.filter((commit: DataPluginCommit) => commit.parents.length <= 1);
+  if (parameters.parametersGeneral.excludeMergeCommits) {
+    commits = commits.filter((commit: DataPluginCommit) => commit.parents.length <= 1);
   }
 
-  if (sortedCommitsByDate.length > 0) {
-    const dataMap: Map<string, { added: number; deleted: number }> = new Map();
-    sortedCommitsByDate.map((sortedCommit: DataPluginCommit) => {
-      // Get all the commits-files connections for the current commit, only those that are related to test files
-      const filteredCommitsFilesConnections: DataPluginCommitsFilesConnection[] = commitsFilesConnections.filter(
-        (commitFileConnection: DataPluginCommitsFilesConnection) =>
-          commitFileConnection._from === sortedCommit._id &&
-          commitFileConnection.action === commitType &&
-          testFiles.some((testFile: DataPluginFile) => testFile._id === commitFileConnection._to),
-      );
-      return filteredCommitsFilesConnections.map((filteredCommitsFilesConnection: DataPluginCommitsFilesConnection) => {
-        return commitsUsersConnections.map((commitsUsersConnection: DataPluginCommitsUsersConnection) => {
-          if (commitsUsersConnection._from === filteredCommitsFilesConnection._from) {
-            if (!dataMap.has(commitsUsersConnection._to)) {
-              dataMap.set(commitsUsersConnection._to, { added: 0, deleted: 0 });
-            }
-            const currentValue: { added: number; deleted: number } = dataMap.get(commitsUsersConnection._to)!;
-            currentValue.added += filteredCommitsFilesConnection.stats.additions;
-            currentValue.deleted += filteredCommitsFilesConnection.stats.deletions;
-            dataMap.set(commitsUsersConnection._to, currentValue);
+  const dataMap: Map<string, { added: number; deleted: number }> = new Map();
+  commits.map((commit: DataPluginCommit) => {
+    // Get all the commits-files connections for the current commit, only those that are related to test files
+    const filteredCommitsFilesConnections: DataPluginCommitsFilesConnection[] = commitsFilesConnections.filter(
+      (commitFileConnection: DataPluginCommitsFilesConnection) =>
+        commitFileConnection._from === commit._id &&
+        commitFileConnection.action === settings.selectedCommitType &&
+        testFiles.some((testFile: DataPluginFile) => testFile._id === commitFileConnection._to),
+    );
+    filteredCommitsFilesConnections.map((filteredCommitsFilesConnection: DataPluginCommitsFilesConnection) => {
+      commitsUsersConnections.map((commitsUsersConnection: DataPluginCommitsUsersConnection) => {
+        if (commitsUsersConnection._from === filteredCommitsFilesConnection._from) {
+          if (!dataMap.has(commitsUsersConnection._to)) {
+            dataMap.set(commitsUsersConnection._to, { added: 0, deleted: 0 });
           }
-        });
+          const currentValue: { added: number; deleted: number } = dataMap.get(commitsUsersConnection._to)!;
+          currentValue.added += filteredCommitsFilesConnection.stats.additions;
+          currentValue.deleted += filteredCommitsFilesConnection.stats.deletions;
+          dataMap.set(commitsUsersConnection._to, currentValue);
+        }
       });
     });
+  });
 
-    const result: TestFileContributorChartData[] = [];
-    dataMap.forEach((value, key) => {
+  const result: TestFileContributorChartData[] = [];
+
+  // Add all selected authors that have contributions to the test files
+  dataMap.forEach((value: { added: number; deleted: number }, key: string) => {
+    if (selectedAuthors.some((author: AuthorType) => author.user._id === key)) {
       const user: DataPluginUser | undefined = users.find((user: DataPluginUser) => user._id === key);
-      result.push({
+      result.push(<TestFileContributorChartData>{
+        color: authorList.find((author) => author.user._id === key)?.color.main || '#545454',
         name: user?.gitSignature ?? 'Unknown User',
         value: {
           added: value.added,
           deleted: value.deleted,
         },
       });
-    });
-    return result;
-  }
+    }
+  });
 
-  return [];
+  // Add all contributions of merged authors to their parent author
+  dataMap.forEach((value: { added: number; deleted: number }, key: string) => {
+    const mergedAuthor: AuthorType | undefined = mergedAuthors.find((author: AuthorType) => author.user._id === key);
+    if (!mergedAuthor) return;
+    const parentAuthor: AuthorType | undefined = authorList.find((author: AuthorType) => author.id === mergedAuthor.parent);
+    if (!parentAuthor) return;
+    const parent: DataPluginUser | undefined = users.find((user: DataPluginUser) => user._id === parentAuthor.user._id);
+    if (!parent) return;
+    const parentEntry: TestFileContributorChartData | undefined = result.find(
+      (entry: TestFileContributorChartData) => entry.name === parent?.gitSignature,
+    );
+    if (parentEntry) {
+      parentEntry.value.added += value.added;
+      parentEntry.value.deleted += value.deleted;
+    }
+  });
+  return result;
 }
