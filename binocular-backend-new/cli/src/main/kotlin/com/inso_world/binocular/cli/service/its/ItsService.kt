3bfb91a80bee5be4ca0a3c991040fdb07f512856
kkastenmeier <kai.kastenmeier@gmail.com>
2025-09-15 19:33:45 +0200

    #330 Added temporary index issue function.

--- binocular-backend-new/cli/src/main/kotlin/com/inso_world/binocular/cli/service/its/ItsService.kt
+++ binocular-backend-new/cli/src/main/kotlin/com/inso_world/binocular/cli/service/its/ItsService.kt
@@ -3,6 +3,7 @@ package com.inso_world.binocular.cli.service.its
 import com.inso_world.binocular.cli.service.VcsService
 import com.inso_world.binocular.github.service.GitHubService
 import com.inso_world.binocular.model.Account
+import com.inso_world.binocular.model.Issue
 import com.inso_world.binocular.model.Project
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -38,6 +39,16 @@ class ItsService(
                 } else {
                     Mono.just(existing.toList())
                 }
+            }
+    }
+
+    //TODO index issues
+    fun indexIssuesFromGitHub(owner: String, repo: String): Mono<List<Issue>> {
+        logger.info("Indexing issues from GitHub for $owner/$repo")
+
+        return gitHubService.loadIssuesWithEvents(owner, repo)
+            .map { issues ->
+                issues.map { it.toDomain() }
             }
     }
 }
