32bfd6e349d1bf6f36e679ad7f5b05cb4699c2bd
FreshGuy32 <mathias.trapl@gmail.com>
2025-09-30 15:09:33 +0200

    #336 Remove not needed dataConverter

--- binocular-frontend-new/src/plugins/visualizationPlugins/sprints/src/chart/Chart.tsx
+++ binocular-frontend-new/src/plugins/visualizationPlugins/sprints/src/chart/Chart.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { DataState, type IssuesState } from '../reducer';
 import type { SprintSettings } from '../settings/settings.tsx';
@@ -8,15 +8,6 @@ import { SprintChart } from './SprintChart.tsx';
 import type { DataPluginIssue } from '../../../../interfaces/dataPluginInterfaces/dataPluginIssues.ts';
 import { getDataSlice } from '../../../simpleVisualizationPlugin/src/reducer/index.ts';
 import moment from 'moment';
-
-export interface ChartData {
-  date: number;
-  [signature: string]: number;
-}
-
-export interface Palette {
-  [signature: string]: { main: string; secondary: string };
-}
 
 const Chart = (
   props: VisualizationPluginProperties<SprintSettings, DataPluginIssue>,
@@ -41,52 +32,20 @@ const Chart = (
   const dataState = useSelector<RootState, IssuesState['dataState']>(
     (state: RootState) => state.plugin.dataState,
   );
-  //React Component State
-  const [chartWidth, setChartWidth] = useState(100);
-  const [chartHeight, setChartHeight] = useState(100);
-
-  const [chartData, setChartData] = useState<ChartData[]>([]);
-  const [_chartScale, setChartScale] = useState<number[]>([]);
-  const [_chartPalette, setChartPalette] = useState<Palette>({});
 
   /**
    * RESIZE Logic START
    */
-  function resize() {
-    if (!props.chartContainerRef?.current) return;
-    if (props.chartContainerRef.current?.offsetWidth !== chartWidth) {
-      setChartWidth(props.chartContainerRef.current.offsetWidth);
-    }
-    if (props.chartContainerRef.current?.offsetHeight !== chartHeight) {
-      setChartHeight(props.chartContainerRef.current.offsetHeight);
-    }
-  }
+  function resize() {}
 
   useEffect(() => {
     resize();
-  }, [props.chartContainerRef, chartHeight, chartWidth]);
+  }, [props.chartContainerRef]);
 
   handelPopoutResizing(props.store, resize);
   /**
    * RESIZE Logic END
    */
-
-  // Effect on data change
-  useEffect(() => {
-    try {
-      if (props.dataConverter) {
-        const { chartData, scale, palette } = props.dataConverter(
-          issues,
-          props,
-        );
-        setChartData(chartData);
-        setChartScale(scale);
-        setChartPalette(palette);
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  }, [issues, props]);
 
   //Set Global state when parameters change. This will also conclude in a refresh of the data.
   useEffect(() => {
@@ -99,9 +58,7 @@ const Chart = (
 
   //Trigger Refresh when dataConnection changes
   useEffect(() => {
-    dispatch({
-      type: 'REFRESH',
-    });
+    dispatch({ type: 'REFRESH' });
   }, [props.dataConnection]);
 
   return (
@@ -117,7 +74,7 @@ const Chart = (
           </div>
         )}
         {dataState === DataState.COMPLETE &&
-          (chartData.length !== 0 ? (
+          (issues.length > 0 || mergeRequests.length > 0 ? (
             <SprintChart
               authors={props.authorList}
               issues={issues}
--- binocular-frontend-new/src/plugins/visualizationPlugins/sprints/src/index.tsx
+++ binocular-frontend-new/src/plugins/visualizationPlugins/sprints/src/index.tsx
@@ -1,9 +1,7 @@
 import PreviewImage from '../assets/thumbnail.svg';
 import Settings, { type SprintSettings } from './settings/settings.tsx';
 import type { VisualizationPlugin } from '../../../interfaces/visualizationPlugin.ts';
-import { getSVGData } from './utilities/utilities.ts';
 import Reducer from './reducer';
-import { convertToChartData } from './utilities/dataConverter.ts';
 import Saga from './saga';
 import Help from './help/help.tsx';
 import type { DataPluginIssue } from '../../../interfaces/dataPluginInterfaces/dataPluginIssues.ts';
@@ -15,14 +13,13 @@ const Sprints: VisualizationPlugin<SprintSettings, DataPluginIssue> = {
   chartComponent: Chart,
   settingsComponent: Settings,
   dataConnectionName: 'issues',
-  dataConverter: convertToChartData,
   helpComponent: Help,
   defaultSettings: {
     showSprints: false,
     coloringMode: 'author',
   },
   export: {
-    getSVGData: getSVGData,
+    getSVGData: () => '',
   },
   capabilities: {
     popoutOnly: false,
