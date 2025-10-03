e0b03eb88c33424825a9e80b185499bf7aff55e1
Manuel Stöger <manuel.stoeger@inso-world.com>
2025-10-03 11:20:14 +0200

    #349: Fixing PerformanceTest

--- binocular-backend-new/ffi/src/test/kotlin/com/inso_world/binocular/ffi/integration/PerformanceTest.kt
+++ binocular-backend-new/ffi/src/test/kotlin/com/inso_world/binocular/ffi/integration/PerformanceTest.kt
@@ -1,6 +1,7 @@
 package com.inso_world.binocular.ffi.integration
 
 import com.inso_world.binocular.ffi.BinocularFfi
+import com.inso_world.binocular.model.Branch
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
@@ -8,11 +9,12 @@ import org.junit.jupiter.api.assertTimeout
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import java.time.Duration
+import kotlin.io.path.Path
 
 @Disabled
 class PerformanceTest {
     private val ffi: BinocularFfi = BinocularFfi()
-    private val repo = ffi.findRepo("../../.git")
+    private val repo = ffi.findRepo(Path("../../.git"))
 
     @ParameterizedTest
     @CsvSource(
@@ -24,10 +26,12 @@ class PerformanceTest {
         "origin/feature/backend-new-gha,2238,65"
     )
     fun `test branch`(branch: String, expectedCommits: Int, timeout: Long) {
+        val branch = Branch(name = branch)
+        this.repo.branches.add(branch)
         val results =
             assertTimeout(
                 Duration.ofMillis(timeout)
-            ) { this.ffi.traverseBranch(repo, branch) }
+            ) { this.ffi.traverseBranch(repo,branch) }
 
         assertThat(results).hasSize(expectedCommits)
     }
