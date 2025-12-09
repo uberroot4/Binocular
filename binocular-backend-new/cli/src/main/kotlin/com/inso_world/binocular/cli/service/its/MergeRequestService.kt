716c1f21d897cf5d77bd7ef02e70d441f52e63a3
kkastenmeier <kai.kastenmeier@gmail.com>
2025-12-09 21:56:14 +0100

    #330 Temporarily removed MergeRequestService as sql implementation is gone since merge.

--- binocular-backend-new/cli/src/main/kotlin/com/inso_world/binocular/cli/service/its/MergeRequestService.kt
+++ binocular-backend-new/cli/src/main/kotlin/com/inso_world/binocular/cli/service/its/MergeRequestService.kt
@@ -5,9 +5,10 @@ import com.inso_world.binocular.model.MergeRequest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
-@Service
-class MergeRequestService(
-    @Autowired private val dao: MergeRequestInfrastructurePort,
-) {
-    fun getOrCreate(e: MergeRequest): MergeRequest = e.id?.let { this.dao.findById(it) } ?: this.dao.create(e)
-}
+// TODO can be uncommented when port is implemented in sql infra
+//@Service
+//class MergeRequestService(
+//    @Autowired private val dao: MergeRequestInfrastructurePort,
+//) {
+//    fun getOrCreate(e: MergeRequest): MergeRequest = e.id?.let { this.dao.findById(it) } ?: this.dao.create(e)
+//}
