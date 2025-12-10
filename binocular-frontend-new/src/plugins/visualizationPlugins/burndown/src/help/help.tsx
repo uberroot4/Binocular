c7536ffcb4313f3f869f1f9d90703ef368285120
FreshGuy32 <mathias.trapl@gmail.com>
2025-12-10 15:53:43 +0100

    #336 Update BurndownChart help

--- binocular-frontend-new/src/plugins/visualizationPlugins/burndown/src/help/help.tsx
+++ binocular-frontend-new/src/plugins/visualizationPlugins/burndown/src/help/help.tsx
@@ -1,22 +1,25 @@
-function Help() {
-  return (
-    <>
-      <h2>Burndown Help</h2>
+const Help: React.FC = () => (
+  <>
+    <h2>Burndown Help</h2>
 
-      <h3>Data</h3>
+    <h3>Data</h3>
 
-      <h3>Parameters</h3>
-      <div>
-        <ul className={'list-disc ml-5'}>
-          <li>
-            <span className={'font-bold'}>Show sprints:</span> Overlay the in the dashboard defined sprints onto the chart. Sprints are
-            displayed as rectengulars at the bottom, with vertical lines that denote their borders. They also show aggregated information
-            based on the issues within their timeframe.
-          </li>
-        </ul>
-      </div>
-    </>
-  );
-}
+    <p>
+      The burndown chart visualization shows the trajectory of the open tickets over time. The visualization displayes an ideal line over
+      the selected timespan. The granularity of the selected timespan can be changed in the general parameters.
+    </p>
+
+    <h3>Parameters</h3>
+    <div>
+      <ul className={'list-disc ml-5'}>
+        <li>
+          <span className={'font-bold'}>Show sprints:</span> Overlay the in the dashboard defined sprints onto the chart. Sprints are
+          displayed as rectengulars at the bottom, with vertical lines that denote their borders. They also show aggregated information
+          based on the issues within their timeframe.
+        </li>
+      </ul>
+    </div>
+  </>
+);
 
 export default Help;
