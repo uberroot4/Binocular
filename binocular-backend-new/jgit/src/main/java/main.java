270d6114d4c2a8ad60a41c172b8a290e26665f6f
Hassan Chehadi <e12119831@student.tuwien.ac.at>
2025-12-17 19:28:58 +0100

    Inital Commit

---
+++ binocular-backend-new/jgit/src/main/java/main.java
@@ -0,0 +1,11 @@
+import com.inso_world.binocular.model.Commit;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.internal.JGitText;
+import org.eclipse.jgit.lib.Repository;
+
+public class main {
+    public static void main(String[] args) {
+
+
+    }
+}
