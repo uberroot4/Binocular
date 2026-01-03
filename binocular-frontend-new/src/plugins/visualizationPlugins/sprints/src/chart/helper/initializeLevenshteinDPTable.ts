fc201b257a5e547790c8d30c3b2799be761b47a2
FreshGuy32 <mathias.trapl@gmail.com>
2026-01-03 16:30:55 +0100

    #336 Add comments to initializeLevenshteinDPTable

--- binocular-frontend-new/src/plugins/visualizationPlugins/sprints/src/chart/helper/initializeLevenshteinDPTable.ts
+++ binocular-frontend-new/src/plugins/visualizationPlugins/sprints/src/chart/helper/initializeLevenshteinDPTable.ts
@@ -1,3 +1,17 @@
+/**
+ * Initializes the dynamic programming table used for the levenshtein distance.
+ * @param a Base value
+ * @param b Comparison value
+ * @returns The initialized array of arrays.
+ *          The first row and column are initialized with the number of the index
+ *          All other cells are initialized with `-1`
+ * ```json
+ * [ [0, 1, 2, 3, 4, 5, 6, 7],
+ *     [1, -1, -1, -1, -1, -1, -1, -1],
+ *     [2, -1, -1, -1, -1, -1, -1, -1],
+ *     [3, -1, -1, -1, -1, -1, -1, -1], ]
+ * ```
+ */
 export const initializeLevenshteinDPTable = (a: string, b: string) => {
   const dpTable: number[][] = [];
 
